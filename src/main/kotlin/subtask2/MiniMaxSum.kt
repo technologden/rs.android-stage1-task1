c1f48ecfce5e5dc2956973f1456a776c5e99e834
Denis Drozdov (Home) <drazdou.work@gmail.com>
2020-03-22 13:53:00 +0300

    feat: add solution MinMaxSum

--- src/main/kotlin/subtask2/MiniMaxSum.kt
+++ src/main/kotlin/subtask2/MiniMaxSum.kt
@@ -1,9 +1,18 @@
 package subtask2
 
 class MiniMaxSum {
-
-    // TODO: Complete the following function
     fun getResult(input: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        var minSum = Int.MAX_VALUE
+        var maxSum = Int.MIN_VALUE
+
+        for (i in input){
+            val sum = input.sum() - i
+            if (sum < minSum)
+                minSum = sum
+            if (sum > maxSum)
+                maxSum = sum
+        }
+
+        return intArrayOf(minSum, maxSum)
     }
 }
